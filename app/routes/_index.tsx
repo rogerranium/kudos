435104272857d29bbb198a0ec1e79b17a1b9220f
momo <rogerranium@yahoo.fr>
2023-08-13 20:28:44 +0200

    init project

---
+++ app/routes/_index.tsx
@@ -0,0 +1,17 @@
+import type { V2_MetaFunction } from "@remix-run/cloudflare";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to rogerranium Cloudflare pages</h1>
+     
+    </div>
+  );
+}
---
+++ public/build/_shared/remix_hmr-JZMQYHSN.js
@@ -0,0 +1,9 @@
+import {
+  createHotContext
+} from "/build/_shared/chunk-X43CXG3P.js";
+import "/build/_shared/chunk-UWV35TSL.js";
+import "/build/_shared/chunk-PNG5AS42.js";
+export {
+  createHotContext
+};
+//# sourceMappingURL=/build/_shared/remix_hmr-JZMQYHSN.js.map
